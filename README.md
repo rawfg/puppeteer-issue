3146581439a075f85cf1c35a2fc821aefe385ef2
Rafal Gorczynski <rafal.gorczynski@gmail.com>
2023-07-11 09:01:12 +0200

    MacOS specifics, cleanups

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Windows Chrome version:
 Chrome brower version: 114.0.5735.199
 NodeJS version: v18.16.0
 npm version: 9.5.1
+
+MacOS:
+Chrome browser version: 114.0.5735.133
+NodeJS version: v18.9.1
+npm version: 8.19.1
--- index.mjs
+++ index.mjs
@@ -5,6 +5,7 @@ const browser = await puppeteer.launch({
 	args: ['--single-process'],
 	headless: 'new',
 	channel: 'chrome',
+	executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
 	// executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
 });
 
@@ -12,8 +13,12 @@ const pages = await browser.pages();
 const page = pages && pages.length > 0 ? pages[0] : await browser.newPage();
 await page.setViewport({width: 1920, height: 1080});
 await page.goto('https://google.com', {waitUntil: 'load'});
+
 console.log('Taking a screenshot of Google');
 await page.screenshot({path: './google.png'});
+
 await page.close();
 await browser.close();
+console.log('Exiting...');
 
+process.exit();
